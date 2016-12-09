2d2c12fe4ff97edd7898e051e684d9217668431d
swindy <1330663837@qq.com>
2016-12-09 19:18:39 +0800

    项目初始化

---
+++ qianrushi/qianrushi/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qianrushi.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        //返回登陆页面
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+	}
+}
---
+++ qianrushi/qianrushi/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qianrushi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //
+        // GET: /Register/
+        
+        //返回注册页面
+        public ActionResult Register()
+        {
+            return View();
+        }
+	}
+}
